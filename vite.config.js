a38a51abd0a6597598d18fca3b7e59bb73afecd4
Sean Kennedy <sean@codeserious.com>
2024-09-23 21:36:17 -0400

    Destructure mode from config

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: mode === 'production' ? '/qritters/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '~assets': path.resolve(__dirname, 'src/assets'),
-      '~components': path.resolve(__dirname, 'src/components'),
-      '~utils': path.resolve(__dirname, 'src/utils'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production' ? '/qritters/' : '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '~assets': path.resolve(__dirname, 'src/assets'),
+        '~components': path.resolve(__dirname, 'src/components'),
+        '~utils': path.resolve(__dirname, 'src/utils'),
+      }
     }
   }
 })
